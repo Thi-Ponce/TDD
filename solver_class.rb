55dfa3fb25670901f58213e1b2e8e9134c772e95
Thi-Ponce <thi.pponce@gmail.com>
2022-05-20 12:53:29 -0300

    Add reverse method

--- solver_class.rb
+++ solver_class.rb
@@ -4,4 +4,10 @@ class Solver
       raise ArgumentError, 'InvalidNumber' if !number.is_a?(Integer) || number.negative?()
       factorial(number - 1) * number
     end
+
+    def reverse(string)
+      raise ArgumentError, 'Invalid input' if !string.is_a?(String)
+
+      string.reverse
+    end
 end
